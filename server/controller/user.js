01b82307a7b50dfc70e044edaf4ad1c53d0e3f2b
lucky <lumpsey@gmail.com>
2018-04-15 14:14:06 +0100

    add user controller function to user route

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,13 +1,34 @@
+import users from '../model/user';
+
 const user = {
   signIn(req, res) {
-    res.json({
-      message: 'user signin successfully',
-    });
+    const newUser = {
+      name: req.body.name,
+      password: req.body.password,
+    };
+    for (let i = 0; i < users.length; i += 1) {
+      if (newUser.name === users[i].name && newUser.password === users[i].password) {
+        return res.status(200).json(`${newUser.name} successfully signin`);
+      }
+    }
+    return res.status(400).json('Signin not successful');
   },
   signUp(req, res) {
-    res.json({
-      message: 'user signup successfully',
+    const newUser = {
+      id: req.body.id,
+      name: req.body.name,
+      password: req.body.password,
+      email: req.body.email,
+      phone: req.body.phone,
+    };
+    users.push(newUser);
+    res.status(201).json({
+      user: newUser,
+      message: `${newUser.name} successfully signup`,
     });
+  },
+  listUser(req, res) {
+    res.status(200).json(users);
   },
 };
 
--- server/routes/user.js
+++ server/routes/user.js
@@ -4,5 +4,6 @@ import userController from '../controller/user';
 const user = express.Router();
 user.post('/signin', userController.signIn);
 user.post('/signup', userController.signUp);
+user.get('/', userController.listUser);
 
 export default user;
